8f410e6c30c3cffdb69a6e3573423cc55b981562
forbit tech <forbit.bd@gmail.com>
2021-06-20 16:05:59 +0600

    06/20/21

--- Models/connectionrequest.js
+++ Models/connectionrequest.js
@@ -1,14 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const ConnectionSchema = new Schema({
-  
   device_id: {
     type: Number,
-    
   },
-  vehicle_reg:{
+  vehicle_reg: {
     type: String,
   },
   device_sim: {
@@ -16,36 +13,32 @@ const ConnectionSchema = new Schema({
   },
   customer_phone: {
     type: Number,
-   
   },
-   vehical_model: {
-    type: String
+  vehical_model: {
+    type: String,
   },
-  service_charge:{
-      type:Number
+  monthly_charge: {
+    type: Number,
   },
-  vehicle_type:{
-      type:String
+  vehicle_type: {
+    type: String,
   },
-  customer_email:{
-      type:String,
-     
+  customer_email: {
+    type: String,
   },
-  is_approve:{
-    type:Boolean,
-    default:false
+  is_approve: {
+    type: Boolean,
+    default: false,
   },
-  dealer_id:{
-    type: mongoose.Types.ObjectId,
-    ref:'Delear'
-},
-  create_date:{
-    type:Date,
-    default:Date.now
-}
+  dealer_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Dealer",
+  },
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-
-
-const Connection = mongoose.model('Connection',ConnectionSchema);
+const Connection = mongoose.model("Connection", ConnectionSchema);
 module.exports = Connection;
--- Models/dealer.js
+++ Models/dealer.js
@@ -1,57 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-var uniqid = require('uniqid');
+var uniqid = require("uniqid");
 
 const DealerSchema = new Schema({
   name: {
-    type: String
+    type: String,
   },
-  email:{
-    type:String,
-    required:true
+  email: {
+    type: String,
+    required: true,
   },
   mobile: {
-    type: Number
+    type: Number,
   },
   address: {
-    type: String
+    type: String,
   },
-  organization_name:{
-    type:String
+  organization_name: {
+    type: String,
   },
-   image: {
-    type: String
+  image: {
+    type: String,
   },
-   is_active: {
+  is_active: {
     type: Boolean,
-    default:false
+    default: false,
   },
-  promo_code:{
-   type:String
+  promo_code: {
+    type: String,
   },
-  fcm_token:{
-    type:String
+  fcm_token: {
+    type: String,
   },
-  create_date:{
-    type:Date,
-    default:Date.now
-}
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-
-DealerSchema.pre('save',function(next){
-
-  if(this.promo_code==null){
-    this.promo_code = uniqid()
-  next()
-    
-  }else{
-    return next()
+DealerSchema.pre("save", function (next) {
+  if (this.promo_code == null) {
+    this.promo_code = uniqid();
+    next();
+  } else {
+    return next();
   }
+});
 
-  
-    
-})
-
-const Dealer = mongoose.model('Delear',DealerSchema);
+const Dealer = mongoose.model("Dealer", DealerSchema);
 module.exports = Dealer;
